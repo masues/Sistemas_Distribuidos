8347d09da56f7f5db7ae81139dbba7ad86002b4d
Mario Suárez <masues64@gmail.com>
2020-11-09 00:01:32 -0600

    Documentación de los códigos del Método de Cristian

--- Proyecto_01/Cristian/Cristian_client.py
+++ Proyecto_01/Cristian/Cristian_client.py
@@ -1,9 +1,19 @@
+#
+# Sistemas Distribuidos, Proyecto 1
+# Programa Cristian_client.py
+# Fecha de creación: 5 de noviembre, 2020
+# Última modificación: 5 de noviembre, 2020
+#
+#
+
 import json 
 from random import randint as ri
 from datetime import datetime as dt
 import time 
 import zmq  
 
+# Función que permite obtener una lista que contiene una hora aleatoria
+# Se establece un valor variando los minutos y segundos de la hora actual
 def randomHour():
 	time = dt.now() 						
 	hour = []
@@ -12,10 +22,12 @@ def randomHour():
 	hour.append(time.second + ri(-9,9))
 	return hour
 
+# Permite dar formato a la lista que representa la hora
 def formatHour(hour):
 	return str(hour[0])+":"+str(hour[1])+":"+str(hour[2])
 
 try:
+	# Lee el archivo que contiene el número de servidores activos
 	stream = open("serv.txt", "r") 
 	info = stream.readline().split()
 	numofserver = info[1]
@@ -23,10 +35,12 @@ try:
 except Exception as exc:
 	print("No se puede abrir el archivo:", exc)
 
+# Establece el contexto y tipo de socket de ZMQ
 context = zmq.Context()
-client = context.socket(zmq.REQ) 
+client = context.socket(zmq.REQ)
 port = 5557
 for i in range(1,int(numofserver)):
+	# Conecta con todos los servidores que tenga disponibles
 	client.connect("tcp://localhost:"+str(port)) 
 	port += 1
 solicitud = 0
@@ -34,6 +48,8 @@ solicitud = 0
 _ = input("Presione enter solo si ya estan activados todos los clientes...")
 
 try:
+	# Reinicia el archivo de servidores para que pueda utilizarse en la siguiente
+	# ejecución del programa
 	stream = open("serv.txt", "w")  
 	stream.write("5557 1")		
 	stream.close()
@@ -43,12 +59,17 @@ except Exception as exc:
 try:
 	while True:
 		_ = input("Presione enter para enviar una solicitud...")
+		# Incrementa el contador de solicitudes para mostrarlo en pantalla
 		solicitud += 1
+		# Obtiene un reloj aleatorio
 		hour = randomHour()
 		print("Enviando solicitud: "+str(solicitud)+"...")
 		for i in range(1,int(numofserver)):
+			# Envía la solicitud al servidor
 			client.send("What is your hour?".encode('utf-8'))
+			# El servidor responde con su hora
 			reply = json.loads(client.recv().decode('utf-8'))
+			# Imprime la hora actualizada
 			print("Hora actual-->"+formatHour(hour)+" Hora actualizada--->"+formatHour(reply)+" del servidor: "+str(reply[3]))
 except KeyboardInterrupt:
 	print("Saliendo")
--- Proyecto_01/Cristian/Cristian_server.py
+++ Proyecto_01/Cristian/Cristian_server.py
@@ -1,8 +1,17 @@
+#
+# Sistemas Distribuidos, Proyecto 1
+# Programa Cristian_server.py
+# Fecha de creación: 5 de noviembre, 2020
+# Última modificación: 5 de noviembre, 2020
+#
+#
+
 import json 
 import time 
 import zmq  
 from datetime import datetime as dt
 
+# Genera una lista que reperesenta al reloj UTC
 def tiempo(server):
 	time = dt.now() 						
 	hour = []
@@ -12,6 +21,8 @@ def tiempo(server):
 	hour.append(server)
 	return hour
 
+# Función que devuelve el siguiente puerto disponible y el siguiente número de
+# servidores
 def update(info):
 	port = int(info[0])
 	port += 1
@@ -20,6 +31,8 @@ def update(info):
 	return str(port)+" "+str(number)
 
 try:
+	# Abre el archivo para leer el puerto que corresponde al servidor actual
+	# y su número identificador
 	stream = open("serv.txt", "r") 
 	info = stream.readline().split()
 	port = info[0]
@@ -30,12 +43,15 @@ except Exception as exc:
 
 
 try:
+	# Actualiza la información del siguiente puerto y número de servidores
+	# disponibles usando la función update
 	stream = open("serv.txt", "w")  
 	stream.write(update(info))		
 	stream.close()
 except Exception as exc:
 	print("No se puede abrir el archivo:", exc)
 
+# Establece el socket con el puerto correspondiente a el servidor actual
 context = zmq.Context()
 server = context.socket(zmq.REP) 
 server.bind("tcp://*:"+port)    
@@ -44,9 +60,13 @@ _ = input("Presione enter para comenzar...")
 
 try:
 	while True:
+		# Recibe la petición del client
 		msg = server.recv().decode('utf-8')
+		# Muestra en pantalla la solicitud del cliente
 		print("Solicitud del cliente --> ",msg)
-		refference = tiempo(numofserver)			
-		server.send(json.dumps(refference).encode('utf-8'))				
+		# Genera una lista con el tiempo UTC del servidor actual
+		refference = tiempo(numofserver)
+		# Envía la información del del tiempo UTC del servidor actual
+		server.send(json.dumps(refference).encode('utf-8'))
 except KeyboardInterrupt:
 	print("Saliendo")
